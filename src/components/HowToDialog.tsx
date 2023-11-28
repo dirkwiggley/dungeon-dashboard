e7692f510a6b929a4b98149d9affa13897528b94
Dirk Wiggley <dirkwiggley@gmail.com>
2023-11-27 19:26:14 -0800

    Fixed a typo
    Fixed a typo

--- src/components/HowToDialog.tsx
+++ src/components/HowToDialog.tsx
@@ -20,7 +20,7 @@ export default function HowToDialog(props: HowToDialogProps) {
             <List>
               <ListItem>Select Button - This button cycles through the various modes (Select, Edit, and Build Room) with the button text describing the current mode.</ListItem>
               <ListItem>Cancel Button - Will undo your current work and switch to Select Mode.</ListItem>
-              <ListItem>Commit Button - Clicking here will take the work you've been doing and save it for the current session. If you're in Edit Mode, it'll update the map tiles. Note if you update a tile in an existing room you will be prompted to see if you want to delete that room. It won't alter the tiles, just the list of tiles that make up an existing room. If you're in Build Room it'll updat the list of rooms with your new room.</ListItem>
+              <ListItem>Commit Button - Clicking here will take the work you've been doing and save it for the current session. If you're in Edit Mode, it'll update the map tiles. Note if you update a tile in an existing room you will be prompted to see if you want to delete that room. It won't alter the tiles, just the list of tiles that make up an existing room. If you're in Build Room it'll update the list of rooms with your new room.</ListItem>
             </List>
           </Box>
           <Divider />
